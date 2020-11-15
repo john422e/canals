de2a189578e146c4ecc3d19cd86bf4b9a9699204
John Eagle <john.p.eagle@gmail.com>
2020-11-15 16:45:55 -0500

    button dev

--- button.py
+++ button.py
@@ -12,11 +12,11 @@ def button_callback(channel):
 
     if button_state == "OFF":
         print("LIGHT ON")
-        GPIO.output(led_pin, GPIO.HIGH)
+        #GPIO.output(led_pin, GPIO.HIGH)
         button_state = "ON"
     else:
         print("LIGHT OFF")
-        GPIO.output(led_pin, GPIO.LOW)
+        #GPIO.output(led_pin, GPIO.LOW)
         button_state = "OFF"
 
 #callback_lambda = lambda x: button_callback(x, button_state)
@@ -28,12 +28,12 @@ GPIO.setmode(GPIO.BCM) # use physical pin numbers
 # set button_pin to be input and set inital value to be pulled low
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # set LED output
-GPIO.setup(led_pin, GPIO.OUT)
+#GPIO.setup(led_pin, GPIO.OUT)
 
 # start up sequence
-GPIO.output(led_pin, GPIO.HIGH)
+#GPIO.output(led_pin, GPIO.HIGH)
 sleep(1)
-GPIO.output(led_pin, GPIO.LOW)
+#GPIO.output(led_pin, GPIO.LOW)
 sleep(1)
 
 # setup event on pin 10 rising edge
